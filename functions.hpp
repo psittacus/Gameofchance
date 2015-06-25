fad06ebc6096c581a59531999ecd56e9317e519c
psittacus <matrixprogrammer@web.de>
2015-06-25 17:47:44 +0200

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,9 @@
+//Games / function 
+void getnwrite ();			//getnwrite
+void lucky_number();		//lucky number
+void guess_before_pc();		//Guess before pc
+void guess_the_number();	//Guess the number
+void lose();				//Verlieren
+void score();				//Score anzeigen
+void setzero();				//Score auf 0 setzen
+void refill();				//Score auf 100 setzen
